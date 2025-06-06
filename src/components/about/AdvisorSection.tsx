68ef528fb97c7b830c51d0d5f9bc520d0beee117
Nishtha Kalra <nishtha.kalra25@gmail.com>
2025-06-06 22:16:26 +0100

    feat: update advisor description

--- src/components/about/AdvisorSection.tsx
+++ src/components/about/AdvisorSection.tsx
@@ -21,7 +21,7 @@ export const AdvisorSection: React.FC = () => {
           <div className="w-full max-w-[440px]"><TeamMemberCard
             name="Akshay Kant"
             title="Strategic Advisor – Web3 & IP Economy"
-            bio="Akshay is a product-minded blockchain engineer with over a decade of experience building payment and blockchain solutions for tier-1 financial institutions like Visa and Chainalysis, developing production-grade Web3 solutions. As a former startup CEO who developed an NFT ticketing marketplace and has advised on the architecture for the UK 'Digital Pound' at the Bank of England, he guides Sia.vision's decentralised IP economy and programmable ownership strategy."
+            bio="Akshay is a product-minded blockchain engineer with over a decade of experience building payment and blockchain solutions for tier-1 financial institutions like Visa and Chainalysis, developing production-grade Web3 solutions. As a former startup CEO who developed an NFT ticketing marketplace and has advised on the architecture for the UK Digital Pound at the Bank of England, he guides Sia.vision's decentralised IP economy and programmable ownership strategy."
             imageUrl="/Akshay-Kant.png"
             linkedinUrl="https://www.linkedin.com/in/akshaykant1708/"
           /></div>
@@ -29,7 +29,7 @@ export const AdvisorSection: React.FC = () => {
           <div className="w-full max-w-[440px]"><TeamMemberCard
             name="Janu Verma"
             title="Technology Advisor – AI & Narrative Intelligence"
-            bio="Janu is a Principal Applied Scientist at Microsoft, leading efforts in building Microsoft Copilot and LLMs, and leveraging graph neural networks and transformer models for contextual content recommendation. With over a decade of experience in applied ML research and building scalable AI products for personalisation and NLP at global tech firms like Mastercard and IBM, Janu advises Sia.vision on ethically integrating cutting-edge AI for narrative generation, content personalisation, and platform intelligence."
+            bio="Janu has over a decade of experience in applied ML research and building scalable AI products for personalisation and NLP at global tech firms like Mastercard and IBM. He is currently leading applied science efforts in building M365 Copilot at Microsoft. Janu advises Sia.vision on ethically integrating cutting-edge AI for narrative generation, content personalisation, and platform intelligence."
             imageUrl="/Janu-Verma.png"
             linkedinUrl="https://www.linkedin.com/in/janu-verma-b79b8823/"
           /></div>
